3a3f4265ec6dbc371d8da97872aaeee292f25262
Siva Reddy <siva@sivareddy.in>
2016-07-04 16:41:32 +0100

    NLP piplined with deplambda

--- src/deplambda/others/NlpPipeline.java
+++ src/deplambda/others/NlpPipeline.java
@@ -24,12 +24,17 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import deplambda.parser.TreeTransformerMain;
+import deplambda.util.TransformationRuleGroups;
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.cornell.cs.nlp.spf.mr.lambda.FlexibleTypeComparator;
+import edu.cornell.cs.nlp.spf.mr.lambda.LogicLanguageServices;
+import edu.cornell.cs.nlp.spf.mr.language.type.MutableTypeRepository;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.pipeline.Annotation;
@@ -58,19 +63,78 @@ public class NlpPipeline extends ProcessStreamInterface {
   public static String NEW_LINE_IS_SENTENCE_BREAK =
       "ssplit.newlineIsSentenceBreak";
 
+  public static String DEPLAMBDA = "deplambda";
+  public static String DEPLAMBDA_LEXICALIZE_PREDICATES =
+      "deplambda.lexicalizePredicates";
+  public static String DEPLAMBDA_TREE_TRANSFORMATIONS_FILE =
+      "deplambda.treeTransformationsFile";
+  public static String DEPLAMBDA_DEFINED_TYPES_FILE =
+      "deplambda.definedTypesFile";
+  public static String DEPLAMBDA_RELATION_PRORITIES_FILE =
+      "deplambda.relationPrioritiesFile";
+  public static String DEPLAMBDA_LAMBDA_ASSIGNMENT_RULES_FILE =
+      "deplambda.lambdaAssignmentRulesFile";
+  public static String DEPLAMBDA_DEBUG = "deplambda.debug";
+
   private StanfordCoreNLP pipeline;
   private Map<String, String> options;
   private Set<String> annotators;
   ConcurrentMaltParserModel maltModel = null;
   RenderSVG svgRenderer = null;
+  TreeTransformerMain treeTransformer = null;
 
   public NlpPipeline(Map<String, String> options) {
+    System.err.println(options);
     this.options = options;
+    
+    if (options.containsKey(DEPLAMBDA)) {
+      System.err.println("Loading DepLambda Model.. ");
+      try {
+        MutableTypeRepository types = new MutableTypeRepository(
+            options.get(DEPLAMBDA_DEFINED_TYPES_FILE));
+        System.err.println(String.format("%s=%s", DEPLAMBDA_DEFINED_TYPES_FILE,
+            options.get(DEPLAMBDA_DEFINED_TYPES_FILE)));
+
+        LogicLanguageServices
+            .setInstance(new LogicLanguageServices.Builder(types,
+                new FlexibleTypeComparator()).closeOntology(false)
+                    .setNumeralTypeName("i").build());
+
+        TransformationRuleGroups treeTransformationRules;
+        treeTransformationRules = new TransformationRuleGroups(
+            options.get(DEPLAMBDA_TREE_TRANSFORMATIONS_FILE));
+        System.err
+            .println(String.format("%s=%s", DEPLAMBDA_TREE_TRANSFORMATIONS_FILE,
+                options.get(DEPLAMBDA_TREE_TRANSFORMATIONS_FILE)));
+
+        TransformationRuleGroups relationPrioritiesRules =
+            new TransformationRuleGroups(
+                options.get(DEPLAMBDA_RELATION_PRORITIES_FILE));
+        System.err
+            .println(String.format("%s=%s", DEPLAMBDA_RELATION_PRORITIES_FILE,
+                options.get(DEPLAMBDA_RELATION_PRORITIES_FILE)));
+
+        TransformationRuleGroups lambdaAssignmentRules =
+            new TransformationRuleGroups(
+                options.get(DEPLAMBDA_LAMBDA_ASSIGNMENT_RULES_FILE));
+        System.err.println(
+            String.format("%s=%s", DEPLAMBDA_LAMBDA_ASSIGNMENT_RULES_FILE,
+                options.get(DEPLAMBDA_LAMBDA_ASSIGNMENT_RULES_FILE)));
+        Boolean lexicalizePredicates =
+            Boolean.parseBoolean(options.getOrDefault(DEPLAMBDA_LEXICALIZE_PREDICATES, "true"));
+
+        treeTransformer = new TreeTransformerMain(treeTransformationRules,
+            relationPrioritiesRules, lambdaAssignmentRules, null,
+            lexicalizePredicates);
+        System.err.println("Loaded DepLambda Model.. ");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
 
     Properties props = new Properties();
-    annotators =
-        Arrays.asList(options.get(ANNOTATORS_KEY).split(",")).stream()
-            .map(String::trim).collect(Collectors.toSet());
+    annotators = Arrays.asList(options.get(ANNOTATORS_KEY).split(",")).stream()
+        .map(String::trim).collect(Collectors.toSet());
 
     if (annotators.contains(STANFORD_DEP_PARSER_KEY)) {
       // Stanford parser picks up default parsing model if
@@ -80,7 +144,7 @@ public class NlpPipeline extends ProcessStreamInterface {
 
     options.entrySet().stream()
         .forEach(option -> props.put(option.getKey(), option.getValue()));
-    
+
     System.err.println("NlpPipeline Specified Options : " + props);
     pipeline = new StanfordCoreNLP(props);
 
@@ -97,8 +161,8 @@ public class NlpPipeline extends ProcessStreamInterface {
     if (options.containsKey(DRAW_SVG_TREES)) {
       svgRenderer = new RenderSVG();
       if (options.containsKey(SVG_ZOOM_FACTOR)) {
-        svgRenderer.setZoomInFactor(Double.parseDouble(options
-            .get(SVG_ZOOM_FACTOR)));
+        svgRenderer
+            .setZoomInFactor(Double.parseDouble(options.get(SVG_ZOOM_FACTOR)));
       }
     }
   }
@@ -109,36 +173,33 @@ public class NlpPipeline extends ProcessStreamInterface {
     JsonArray words;
     if (jsonSentence.has(SentenceKeys.WORDS_KEY)) {
       words = jsonSentence.get(SentenceKeys.WORDS_KEY).getAsJsonArray();
-      Preconditions
-          .checkArgument(options.containsKey(WHITESPACE_TOKENIZER)
+      Preconditions.checkArgument(
+          options.containsKey(WHITESPACE_TOKENIZER)
               && options.get(WHITESPACE_TOKENIZER).equals("true"),
-              "words are already tokenized. You should use tokenize.whitespace=true");
+          "words are already tokenized. You should use tokenize.whitespace=true");
 
-      Preconditions
-          .checkArgument(options.containsKey(WHITESPACE_TOKENIZER)
+      Preconditions.checkArgument(
+          options.containsKey(WHITESPACE_TOKENIZER)
               && options.get(WHITESPACE_TOKENIZER).equals("true"),
-              "input is already tokenized. You should use tokenize.whitespace=true");
+          "input is already tokenized. You should use tokenize.whitespace=true");
 
       Preconditions
           .checkArgument(
-              (options.containsKey(SENTENCE_EOL_SPLITTER) && options.get(
-                  SENTENCE_EOL_SPLITTER).equals("true"))
-                  || (options.containsKey(NEW_LINE_IS_SENTENCE_BREAK) && options
-                      .get(NEW_LINE_IS_SENTENCE_BREAK).equals("always")),
-              "input is already tokenized. You should either use "
-                  + "ssplit.eolonly=true if you have a single sentence or"
-                  + " ssplit.newlineIsSentenceBreak=always if there are multiple sentences");
+              (options.containsKey(SENTENCE_EOL_SPLITTER) && options
+                  .get(SENTENCE_EOL_SPLITTER).equals("true"))
+          || (options.containsKey(NEW_LINE_IS_SENTENCE_BREAK)
+              && options.get(NEW_LINE_IS_SENTENCE_BREAK).equals("always")),
+          "input is already tokenized. You should either use "
+              + "ssplit.eolonly=true if you have a single sentence or"
+              + " ssplit.newlineIsSentenceBreak=always if there are multiple sentences");
 
       StringBuilder sb = new StringBuilder();
-      jsonSentence
-          .get(SentenceKeys.WORDS_KEY)
-          .getAsJsonArray()
-          .forEach(
-              word -> {
-                sb.append(word.getAsJsonObject().get(SentenceKeys.WORD_KEY)
-                    .getAsString());
-                sb.append(" ");
-              });
+      jsonSentence.get(SentenceKeys.WORDS_KEY).getAsJsonArray()
+          .forEach(word -> {
+            sb.append(word.getAsJsonObject().get(SentenceKeys.WORD_KEY)
+                .getAsString());
+            sb.append(" ");
+          });
       sentence = sb.toString().trim();
     } else {
       sentence = jsonSentence.get(SentenceKeys.SENTENCE_KEY).getAsString();
@@ -148,15 +209,14 @@ public class NlpPipeline extends ProcessStreamInterface {
     Annotation annotation = new Annotation(sentence);
     pipeline.annotate(annotation);
     int wordCount = 0;
-    for (CoreMap sentenceAnnotation : annotation.get(SentencesAnnotation.class)) {
+    for (CoreMap sentenceAnnotation : annotation
+        .get(SentencesAnnotation.class)) {
       int sentStart = wordCount;
       for (CoreLabel token : sentenceAnnotation.get(TokensAnnotation.class)) {
         JsonObject wordObject;
         if (jsonSentence.has(SentenceKeys.WORDS_KEY)) {
-          Preconditions
-              .checkArgument(
-                  wordCount < words.size(),
-                  "Inconsistent number of already tokenized words, and newly tokenized words. Remove the key 'words' and try again");
+          Preconditions.checkArgument(wordCount < words.size(),
+              "Inconsistent number of already tokenized words, and newly tokenized words. Remove the key 'words' and try again");
           wordObject = words.get(wordCount).getAsJsonObject();
         } else {
           wordObject = new JsonObject();
@@ -181,9 +241,8 @@ public class NlpPipeline extends ProcessStreamInterface {
       }
 
       if (annotators.contains(STANFORD_DEP_PARSER_KEY)) {
-        SemanticGraph depTree =
-            sentenceAnnotation
-                .get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+        SemanticGraph depTree = sentenceAnnotation.get(
+            SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
         for (IndexedWord root : depTree.getRoots()) {
           int rootIndex = sentStart + root.index();
           JsonObject rootWord = words.get(rootIndex - 1).getAsJsonObject();
@@ -219,13 +278,15 @@ public class NlpPipeline extends ProcessStreamInterface {
       for (JsonElement word : words) {
         currentWordIndex++;
         JsonObject currentWord = word.getAsJsonObject();
-        sentTokens.add(String.format("%d\t%s\t_\t%s\t_\t_", currentWordIndex
-            - sentStart + 1, currentWord.get(SentenceKeys.WORD_KEY)
-            .getAsString(), currentWord.has(SentenceKeys.POS_KEY) ? currentWord
-            .get(SentenceKeys.POS_KEY).getAsString() : "_"));
+        sentTokens.add(String
+            .format("%d\t%s\t_\t%s\t_\t_", currentWordIndex - sentStart + 1,
+                currentWord.get(SentenceKeys.WORD_KEY).getAsString(),
+                currentWord.has(SentenceKeys.POS_KEY)
+                    ? currentWord.get(SentenceKeys.POS_KEY).getAsString()
+                    : "_"));
 
-        if ((currentWord.has(SentenceKeys.SENT_END) && currentWord.get(
-            SentenceKeys.SENT_END).getAsBoolean())
+        if ((currentWord.has(SentenceKeys.SENT_END)
+            && currentWord.get(SentenceKeys.SENT_END).getAsBoolean())
             || currentWordIndex + 1 == words.size()) {
           try {
             String[] sentTokensArr = new String[sentTokens.size()];
@@ -265,19 +326,25 @@ public class NlpPipeline extends ProcessStreamInterface {
         e.printStackTrace();
       }
     }
+
+    if (options.containsKey(DEPLAMBDA)) {
+      System.err.println("Runnning deplambda");
+      treeTransformer.processSentence(jsonSentence);
+    }
   }
 
-  public static void main(String[] args) throws IOException,
-      InterruptedException {
+  public static void main(String[] args)
+      throws IOException, InterruptedException {
     if (args.length == 0 || args.length % 2 != 0) {
-      System.err.println("Specify pipeline arguments, e.g., annotator, languageCode. See the NlpPipelineTest file.");
+      System.err.println(
+          "Specify pipeline arguments, e.g., annotator, languageCode. See the NlpPipelineTest file.");
     }
-    
+
     Map<String, String> options = new HashMap<>();
     for (int i = 0; i < args.length; i += 2) {
       options.put(args[i], args[i + 1]);
     }
-    
+
     NlpPipeline englishPipeline = new NlpPipeline(options);
     englishPipeline.processStream(System.in, System.out, 20, true);
 
