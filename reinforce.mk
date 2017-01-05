879a961979c8bf9dab22eb832c176db6c8fc5f49
Siva Reddy <siva@sivareddy.in>
2017-01-05 13:31:33 +0000

    bug in reinforce.ml

--- reinforce.mk
+++ reinforce.mk
@@ -2,6 +2,6 @@ dump_bow_graphs_%:
 	java -cp bin:lib/* in.sivareddy.scripts.DumpBoWGroundedGraphs \
 		data/freebase/schema/all_domains_schema.txt \
 		buck \
-		10
-		< ../deplambda/data/webquestions/en/en-bilty-bist-webquestions.$*.deplambda.json
-	> working/wq.bow.graphs.$*.json
+		20 \
+		< ../udeplambda/data/webquestions/en/en-bilty-bist-webquestions.$*.deplambda.json \
+		> working/wq.bow.graphs.$*.json
